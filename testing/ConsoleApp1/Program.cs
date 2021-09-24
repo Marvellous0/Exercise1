4175113484981d393510622ff58b86252d143e2f
Marvellous <adeoyemarvellous7@gmail.com>
2021-09-24 20:09:59 +0100

    .....

---
+++ testing/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int[,] arr = 
+            {
+                {10, 2, 9}, 
+                {4, 8, 6}, 
+                {12, 8, 9}
+            };
+
+            int sum1 = 0,
+            sum2 = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                sum1 += arr[i, i];
+                
+                sum2 += arr[i, n-i-1];
+                
+            }
+            Console.WriteLine(Math.Abs(sum1));
+            Console.WriteLine(Math.Abs(sum2));
+            Console.WriteLine(Math.Abs(sum1 - sum2));
+        }
+    }
+}
